42ae94bfd707c9735920d853e88b16d0ccc933ae
Sebastian Boldt <self.dealloc@googlemail.com>
2017-05-23 23:23:44 +0200

    inital setup - again

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -16,10 +16,22 @@ import App
 ///
 /// .run() runs the Droplet's commands, 
 /// if no command is given, it will default to "serve"
+
+/*
+ The Droplet is a service container that gives you access to many of Vapor's facilities. 
+ It is responsible for registering routes, starting the server, appending middleware, and more. */
+
 let config = try Config()
 try config.setup()
+
+/* 
+ The environment affects Config and Logging. The environment is development by default. To change it, pass the --env= flag as an argument. 
+*/
+
+print("You are currently running on", config.environment.description.uppercased())
 
 let drop = try Droplet(config)
 try drop.setup()
 
 try drop.run()
+
