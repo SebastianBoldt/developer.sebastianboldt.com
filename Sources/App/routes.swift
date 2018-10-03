ed4a32c0f99ddc5e541e87be00c695f09f187cce
Sebastian Boldt <self.dealloc@googlemail.com>
2018-10-03 14:59:32 +0200

    initial steps for rewrite of new website

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,20 +1,16 @@
 import Vapor
 
-/// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
     router.get { req in
         return "It works!"
     }
     
     // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.get("developer") { req in
+        return "Developer"
     }
-
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    
+    router.get("music") { req in
+        return "Music"
+    }
 }
