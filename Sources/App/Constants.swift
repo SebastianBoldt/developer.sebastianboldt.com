0811aa6945722cddad35b4c3a098a3dae1873874
Sebastian Boldt <self.dealloc@googlemail.com>
2019-11-09 09:02:18 +0100

    added a memoji to my page; added new blog posts etc.

--- Sources/App/Constants.swift
+++ Sources/App/Constants.swift
@@ -14,7 +14,29 @@ public struct Constants {
     }
     
     public struct Seeds {
-        public static let skills = ["Mobile Development", "iOS", "Swift", "Objective-C", "Software Engineering", "Multithreaded Programming", "Xcode Professional", "GoF - Gang of Four Design Patterns", "Code Signing and Provisioning", "Cocoapods", "Carthage", "Fastlane", "Git", "Gitflow Workflow", "RxSwift", "MVC / MVVM / VIPER", "Unit & UI Testing", "Continuous Integration", "Realm", "Vapor", "Server-side Swift"]
+        public static let skills = ["Mobile Development",
+                                    "iOS",
+                                    "Swift",
+                                    "SwiftUI",
+                                    "Combine",
+                                    "Objective-C",
+                                    "Software Engineering",
+                                    "Multithreaded Programming",
+                                    "Xcode Professional",
+                                    "GoF - Gang of Four Design Patterns",
+                                    "Code Signing and Provisioning",
+                                    "Cocoapods",
+                                    "Carthage",
+                                    "Fastlane",
+                                    "Git",
+                                    "Gitflow Workflow",
+                                    "RxSwift",
+                                    "MVC / MVVM / VIPER",
+                                    "Unit & UI Testing",
+                                    "Continuous Integration",
+                                    "Realm",
+                                    "Vapor",
+                                    "Server-side Swift"]
         
     }
 }
