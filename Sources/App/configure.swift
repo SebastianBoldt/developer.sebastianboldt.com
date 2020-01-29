e0277d833089d384c9fade8234d05641679dd332
Sebastian Boldt <self.dealloc@googlemail.com>
2020-01-29 13:16:52 +0100

    heroku preparations

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -16,5 +16,6 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+    middlewares.use(XFPMiddleware()) //
     services.register(middlewares)
 }
