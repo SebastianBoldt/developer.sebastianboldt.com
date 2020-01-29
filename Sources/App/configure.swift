2dbe5d3e34aec5d1e118d39e0a79874e19ca0af7
Sebastian Boldt <self.dealloc@googlemail.com>
2020-01-29 13:24:36 +0100

    heroku preparations

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,7 +7,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
-
+    
     // Register Lead Templating Engine
     try services.register(LeafProvider())
     config.prefer(LeafRenderer.self, for: ViewRenderer.self)
@@ -16,6 +16,5 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
-    middlewares.use(XFPMiddleware()) //
     services.register(middlewares)
 }
